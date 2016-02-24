50b3fcd45b972e23a06d64aec8fb29a0a261cbeb
paulsiskind <paulsiskind@gmail.com>
2016-02-24 13:29:55 -0700

    working on portfolio

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,8 +1,14 @@
-$(document).ready(function(){$("nav a").click(function(evn){
+$(document).ready(function(){
+       $("nav a").click(function(evn){
             evn.preventDefault();
             $('html,body').scrollTo(this.hash, this.hash); 
         });
         
+
+        $('#portfolioContainer img').click(function(){
+            alert('tacos');
+            (this).css("color","blue");
+        })
         
    
     });
