1ad2d7ce45814c96ffc8e37f0d8c9512d2146986
paulsiskind <paulsiskind@gmail.com>
2016-11-23 12:17:10 -0700

    Fixed Parallax Scroll

--- public/javascripts/controllers/homepagecontroller.js
+++ public/javascripts/controllers/homepagecontroller.js
@@ -33,5 +33,19 @@ app.controller('HomePageController', function ($scope, $http, $routeParams) {
     $scope.downRiverbackground= !$scope.downRiverbackground
   }
 
+  var $window = $(window); 
+  $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this); // assigning the object
+    
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+            
+            // Put together our final background position
+            var coords = '50% '+ yPos + 'px';
+
+            // Move the background
+            $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });  
 
 });
