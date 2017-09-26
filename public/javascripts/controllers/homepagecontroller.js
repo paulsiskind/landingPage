6cb56966f77fc252e167ca044e9647ee5519e344
paulsiskind <paulsiskind@gmail.com>
2017-09-26 12:33:43 -0400

    Update

--- public/javascripts/controllers/homepagecontroller.js
+++ public/javascripts/controllers/homepagecontroller.js
@@ -1,37 +1,5 @@
 app.controller('HomePageController', function ($scope, $http, $routeParams) {
    
-  $scope.thepowdrbackground = false;
-  $scope.theRiverbackground = false;
-  $scope.urbanSherpabackground = false;
-  $scope.gSchoolbackground = false;
-  $scope.redditClonebackground = false;
-  $scope.downRiverbackground = false;
-
-  $scope.theRiver = function(){
-    console.log($scope.theRiverbackground)
-    $scope.theRiverbackground = !$scope.theRiverbackground;
-  }
-
-  $scope.thepowdr = function(){
-    $scope.thepowdrbackground = !$scope.thepowdrbackground;
-  }
-
-  $scope.urbanSherpa = function(){
-    console.log('urbanSherpabackground')
-    $scope.urbanSherpabackground = !$scope.urbanSherpabackground;
-  }
-
-  $scope.gSchool = function(){
-    $scope.gSchoolbackground = !$scope.gSchoolbackground
-  }
-
-  $scope.redditClone = function(){
-    $scope.redditClonebackground = !$scope.redditClonebackground
-  }
-
-   $scope.downRiver = function(){
-    $scope.downRiverbackground= !$scope.downRiverbackground
-  }
   var controller = new ScrollMagic.Controller();
   
   new ScrollMagic.Scene({
